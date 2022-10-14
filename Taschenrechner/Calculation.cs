ad061b1cd3f376e92ed07f895bd7a851950cd8a3
torge <torge.stubbe@fricke.de>
2022-10-14 10:57:27 +0200

    init

---
+++ Taschenrechner/Calculation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+namespace Taschenrechner
+{
+    public class Calculation
+    {
+    
+        private decimal value, operand2, operand1= 0;
+        
+        public enum Method
+        {
+            NotSet,
+            Add,
+            Substract,
+            Multiply,
+            Divide
+        }
+        public Method method;
+
+
+        public void reset()
+        {
+            this.operand2 = 0;
+            this.operand1 = 0;
+            this.value = 0;
+        }
+
+
+        public void setOperand1(String inputString)
+        {
+            this.operand1 = convertToDecimal(inputString);
+        }
+
+        public void setOperand2(String inputString)
+        {
+            this.operand2 = convertToDecimal(inputString);
+        }
+
+        public String getVal()
+        {
+            return this.value.ToString();
+        }
+
+        public void calculate()
+        {
+            switch (this.method)
+            {
+                case Method.Add:
+                    {
+                        this.add();
+                        break;
+                    }
+                case Method.Substract:
+                    {
+                        this.substract();
+                        break;
+                    }
+                case Method.Multiply:
+                    {
+                        this.multiply();
+                        break;
+                    }
+                case Method.Divide:
+                    {
+                        this.divide();
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            this.operand1 = this.value;
+
+        }
+
+        private void add()
+        {
+            this.value = this.operand1 + this.operand2;
+        }
+
+        private void substract(){
+            this.value = this.operand1 - this.operand2;
+        }
+
+        private void multiply()
+        {
+            this.value = this.operand1 * this.operand2;
+
+        }
+
+        private void divide()
+        {
+            this.value = this.operand1 / this.operand2;
+        }
+
+        private Decimal convertToDecimal(String inputString)
+        {
+            if (Decimal.TryParse(inputString, out decimal convertedString))
+            {
+                return convertedString;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+
+    }
+}
---
+++ Taschenrechner/MainWindow.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    Taschenrechner.Calculation calculation = new Taschenrechner.Calculation();
+    Boolean isResult = false;
+    Boolean pendingOperation = false;
+
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    //Helper
+    private void clearText()
+    {
+        displayMain.DeleteText(0, displayMain.Text.Length);
+    }
+
+    private String getText()
+    {
+        return displayMain.Text.Replace(",", ".");
+    }
+
+
+
+    //Number Input + comma
+    private void setText(String text)
+    {
+        text = text.Replace(".", ",");
+        displayMain.InsertText(text);
+    }
+
+    protected void ClickNumber(object sender, EventArgs e)
+    {
+        Gtk.Button button = (Gtk.Button)sender;
+        String buttonLabel = button.Label;
+
+        if (isResult)
+            {
+                clearText();
+                isResult = false;
+            }
+            if (buttonLabel == ",")
+            {
+                if (displayMain.Text.Length == 0)
+                {
+                    displayMain.Text = ("0,");
+                }
+                else if (!displayMain.Text.Contains(","))
+                {
+                displayMain.Text += (",");
+                }
+            }
+            else
+            {
+                displayMain.Text += (buttonLabel);
+            }
+    }
+
+
+
+    //deleting
+    protected void clickButtonDel(object sender, EventArgs e)
+    {
+        //displayMain.DeleteText(displayMain.Text.Length - 1, displayMain.Text.Length);
+        if (displayMain.Text.Length > 0)
+        {
+            displayMain.Text = displayMain.Text.Remove(displayMain.Text.Length - 1);
+        }
+    }
+
+    protected void clickButtonReset(object sender, EventArgs e)
+    {
+        clearText();
+        calculation.reset();
+        pendingOperation = false;
+        isResult = false;
+    }
+
+    //result
+    protected void clickButtonEquals(object sender, EventArgs e)
+    {
+        if (pendingOperation)
+        {
+            resolution();
+            pendingOperation = false;
+        }
+        else
+        {
+            if (calculation.method != Taschenrechner.Calculation.Method.NotSet)
+            {
+                resolution();
+            }
+        }
+    }
+
+    private void resolution() {
+
+        if (pendingOperation)
+        {
+            calculation.setOperand2(getText());
+        }
+
+        calculation.calculate();
+        clearText();
+
+        setText(calculation.getVal());
+
+        isResult = true;
+    }
+
+    //math functions
+    private void manageFunction()
+    {
+        if (pendingOperation)
+        {
+            resolution();
+        }
+        else
+        {
+            calculation.setOperand1(getText());
+            clearText();
+            pendingOperation = true;
+        }
+    }
+
+    protected void clickButtonAdd(object sender, EventArgs e)
+    {
+        manageFunction(); 
+        calculation.method = Taschenrechner.Calculation.Method.Add;
+    }
+
+    protected void ClickButtonSubstract(object sender, EventArgs e)
+    {
+        manageFunction();
+        calculation.method = Taschenrechner.Calculation.Method.Substract;
+    }
+
+    protected void ClickButtonMultiply(object sender, EventArgs e)
+    {
+        manageFunction(); 
+        calculation.method = Taschenrechner.Calculation.Method.Multiply;
+    }
+
+    protected void ClickButtonDivide(object sender, EventArgs e)
+    {
+        manageFunction(); 
+        calculation.method = Taschenrechner.Calculation.Method.Divide;
+    }
+
+    protected void ClearEntry(object sender, EventArgs e)
+    {
+        displayMain.DeleteText(0, displayMain.Text.Length);
+    }
+
+    protected void PrependMinus(object sender, EventArgs e)
+    {
+        if (!displayMain.Text.Contains("-"))
+        {
+            //displayMain.PrependText("-");
+            displayMain.Text = displayMain.Text.Insert(0, "-");
+        }
+    }
+}
